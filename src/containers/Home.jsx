a31f44dbd15d341e084ef97ccc2fea68aca0a3bb
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-06-14 22:22:24 -0500

    added the component products in the Home page

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Products from '../components/Products';
+import initialState from '../initialState';
 
 const Home = () => {
-  return <div>Home</div>;
+  return <Products products={initialState.products} />;
 };
 
 export default Home;
