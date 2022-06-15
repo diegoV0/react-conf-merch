9c21ac61466de4160a130669e6c11bf0627db5dc
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-06-14 22:58:28 -0500

    edit the content of the containers  Checkout and Information

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 import '../styles/components/Checkout.css';
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  return (
+    <div className="Checkout">
+      <div className="Ckekcout-content">
+        <h3>Lista de Pedidos:</h3>
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>Item Name</h4>
+            <span>$10</span>
+          </div>
+          <button type="button">Eliminar</button>
+        </div>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Precio Total: $10</h3>
+        <button type="buttom">Continuar Pedido</button>
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -2,7 +2,41 @@ import React from 'react';
 import '../styles/components/Information.css';
 
 const Information = () => {
-  return <div>Information</div>;
+  return (
+    <div className="Information">
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Informacion de contacto:</h2>
+        </div>
+        <div className="Information-form">
+          <form action="">
+            <input type="text" placeholder="Nombre Completo" name="name" />
+            <input type="text" placeholder="Correo Electronico" name="mail" />
+            <input type="text" placeholder="Direccion" name="address" />
+            <input type="text" placeholder="Aapto" name="apto" />
+            <input type="text" placeholder="Cuidad" name="city" />
+            <input type="text" placeholder="Pais" name="country" />
+            <input type="text" placeholder="Estado" name="state" />
+            <input type="text" placeholder="Codigo Postal" name="namcpcpe" />
+            <input type="text" placeholder="Telefono" name="phone" />
+          </form>
+        </div>
+        <div className="Infortation-buttons">
+          <div className="Information-back">Regresar</div>
+          <div className="Information-next">Pagar</div>
+        </div>
+      </div>
+      <div className="Information-sidebar">
+        <h3>Pedido</h3>
+        <div className="Information-item">
+          <div className="Information-element">
+            <h4>Item Name</h4>
+            <span>$100</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Information;
