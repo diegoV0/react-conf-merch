33b16b76053f416453682305667636efe3d7e806
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-06-15 22:44:52 -0500

    added conext and the cuatom hook useInitialState

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -6,22 +6,32 @@ import NotFound from '../containers/NotFound';
 import Payment from '../containers/Payment';
 import Success from '../containers/Success';
 import Layout from '../components/Layout';
+import AppContext from '../context/AppContext';
+import useInitialstate from '../hooks/useInitialSatate';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
+  const initialState = useInitialstate();
+
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/checkout" element={<Checkout />} />{' '}
-          <Route exact path="/checkout/information" element={<Information />} />{' '}
-          <Route exact path="/checkout/payment" element={<Payment />} />
-          <Route exact path="/checkout/success" element={<Success />} />{' '}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/checkout" element={<Checkout />} />{' '}
+            <Route
+              exact
+              path="/checkout/information"
+              element={<Information />}
+            />{' '}
+            <Route exact path="/checkout/payment" element={<Payment />} />
+            <Route exact path="/checkout/success" element={<Success />} />{' '}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
