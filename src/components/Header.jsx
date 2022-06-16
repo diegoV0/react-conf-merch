9a15c5e26392f32cfa849bfe0f886c6a1c0a195a
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-06-15 22:20:40 -0500

    integration of icons and connection with react router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Header.css';
 
 const Header = () => {
   return (
     <div className="Header">
-      <h1 className="Header-title">React Conf Merch</h1>
-      <div className="Header-checkout">Checkout</div>
+      <h1 className="Header-title">
+        <Link to="/">Platzi Conf Merch</Link>
+      </h1>
+      <div className="Header-checkout">
+        <Link to="/checkout">
+          <i className="fas fa-shopping-basket" title="Checkout" />
+        </Link>
+      </div>
     </div>
   );
 };
--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Checkout.css';
 
 const Checkout = () => {
@@ -11,12 +12,16 @@ const Checkout = () => {
             <h4>Item Name</h4>
             <span>$10</span>
           </div>
-          <button type="button">Eliminar</button>
+          <button type="button">
+            <i className="fas fa-trash" title="Eliminar" />
+          </button>
         </div>
       </div>
       <div className="Checkout-sidebar">
         <h3>Precio Total: $10</h3>
-        <button type="buttom">Continuar Pedido</button>
+        <Link to="/checkout/information">
+          <button type="buttom">Continuar Pedido</button>
+        </Link>
       </div>
     </div>
   );
